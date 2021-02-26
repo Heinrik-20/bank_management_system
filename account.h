2e1665731ad0134a45cb5ff8088e5f8224106b12
Heinrik-20 <69374871+Heinrik-20@users.noreply.github.com>
2021-02-26 11:42:54 +1100

    Update account.h

--- account.h
+++ account.h
@@ -1,8 +1,14 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
 typedef struct {
     int id;
+    int pin;
     double amount;
 }account_t;
 
-void print_items(void *data);
+void print_and_free(void *data);
 int compare_id(void *id1, void *id2);
-account_t *create_new_account(int id);
+account_t *create_new_account(int id, int pin);
+
+#endif 
