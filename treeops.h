c583cc2d4d9e6b8f4cb004a0031042a1055fc9e2
Heinrik-20 <69374871+Heinrik-20@users.noreply.github.com>
2021-02-26 11:45:08 +1100

    Update treeops.h

--- treeops.h
+++ treeops.h
@@ -1,3 +1,6 @@
+#ifndef TREEOPS_H
+#define TREEOPS_H
+
 typedef struct node node_t;
 
 struct node {
@@ -13,11 +16,14 @@ typedef struct {
     void (*print) (void*);
 } tree_t;
 
-void print_all_items(node_t *tree, void print(void*));
+void traverse_tree(node_t *tree, void action(void*));
 node_t *node_init(void *data);
 tree_t *tree_init(int cmp(void*, void*), void print(void*));
 int is_empty_tree(tree_t *tree);
-int search_tree(tree_t *tree, void *key);
-int recursive_search(node_t *root, void *key, int cmp(void*, void*));
+node_t *search_tree(tree_t *tree, void *key);
+node_t *recursive_search(node_t *root, void *key, int cmp(void*, void*));
 tree_t *insert_tree(tree_t *tree, void *acc);
 node_t *recursive_insert(node_t *root, node_t *node, int cmp(void*, void*));
+void free_tree(tree_t **tree);
+
+#endif
