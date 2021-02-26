e5f0448b8851264e625ac217c9de119ffb2599a4
Heinrik-20 <69374871+Heinrik-20@users.noreply.github.com>
2021-02-26 11:44:13 +1100

    Update main.c

--- main.c
+++ main.c
@@ -2,22 +2,57 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include "account.h"
 #include "treeops.h"
-
+#include "account.h"
+#include "bankops.h"
 
 int main(int argc, char **argv){
-    int accounts, id;
-    tree_t *all_accounts = tree_init(compare_id, print_items);
-    account_t *acc = NULL;
-    printf("Number of accounts: ");
-    scanf("%d", &accounts);
-    for (int i = 0;i < accounts;i++){
-        printf("ID: ");
-        scanf("%d", &id);
-        acc = create_new_account(id);
-        all_accounts = insert_tree(all_accounts, acc);
+    int action;
+    tree_t *all_accounts = tree_init(compare_id, print_and_free);
+    printf("0: Create new account, 1: Deposit account, 2: Withdrawal 3: View Balance\n");
+    printf("Enter action: ");
+    while (scanf("%d", &action) == 1){
+        int id, pin;
+        account_t *new = NULL;
+        node_t *key = NULL, *acc;
+        switch (action){
+            case 0:
+                printf("Enter New ID: ");
+                scanf("%d", &id);
+                printf("Enter new password: ");
+                scanf("%d", &pin);
+                new = create_new_account(id, pin);
+                all_accounts = insert_tree(all_accounts, new);
+                break;
+            
+            case 1:
+                acc = get_details(all_accounts);
+                if (acc){
+                    acc = do_action(acc, 1, "Deposit");
+                }
+                break;
+
+            case 2:
+                acc = get_details(all_accounts);
+                if (acc){
+                    acc = do_action(acc, -1, "Withdraw");
+                }
+                break;
+
+            case 3:
+                acc = get_details(all_accounts);
+                if (acc) {
+                    printf("Balance available: $%.2lf\n", ((account_t *)(acc->data))->amount);
+                }
+                break;
+
+            default:
+                printf("Invalid action, try again\n");
+                break;
+        }
+        printf("Enter action:\n");
     }
-    print_all_items(all_accounts->root, all_accounts->print);
+    traverse_tree(all_accounts->root, all_accounts->print);
+    free_tree(&all_accounts);
     return 0;
 }
